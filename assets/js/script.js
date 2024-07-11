876b171b13d1d9fb5eb16c7e91b8deb990ad7484
josé Pérez <j.igna.perez@gmail.com>
2024-07-11 19:52:36 -0400

    CSS complete, footer links added, cards same height.... maybe last commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,18 @@ $(window).scroll(function(){
         $("#navbarfijo").addClass("bg-transparent");
     }
 })
+// smooth scroll con js.
+// $(function () {
+//     $("a").click(function (event) {
+//         if (this.hash !== "") {
+//             event.preventDefault();
+//             var gato = this.hash;
+//             $("html, body").animate({
+//                 scrollTop: $(gato).offset().top
+//             }, 800);
+//         }
+//     });
+
+
+   
+// });
